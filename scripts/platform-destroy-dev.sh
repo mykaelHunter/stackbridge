cd437a10dd0145fd94d312c39b3c77c262a06a52
Hunter <msilas007@gmail.com>
2026-07-03 12:47:49 +0100

    Modified platform destroy scripts

--- scripts/platform-destroy-dev.sh
+++ scripts/platform-destroy-dev.sh
@@ -18,5 +18,25 @@ echo ""
 
 stackbridge destroy --env dev --force
 
+echo ""
+
+echo "Deleting kops bucket"
+echo "Press q when you see the : prompt"
+aws s3api delete-objects \
+    --bucket stackbridge-tf-state \
+    --delete "$(aws s3api list-object-versions \
+    --bucket stackbridge-tf-state \
+    --output json \
+    --query '{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"
+
+aws s3api delete-objects \
+    --bucket stackbridge-tf-state \
+    --delete "$(aws s3api list-object-versions \
+    --bucket stackbridge-tf-state \
+    --output json \
+    --query '{Objects: DeleteMarkers[].{Key:Key,VersionId:VersionId}}')"
+
+echo ""
+
 echo "Successful"
 
